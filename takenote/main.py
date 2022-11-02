d9f8f6fd2e6b1d8205f919214a1b64783c97f9c6
proximalf <proximalfuture@gmail.com>
2022-11-02 20:47:49 +0000

    multiline working
    arg note is note required
    
    changed prints to click.echos
    
    wrapped in a try statement as a lazy way of catching errors

--- takenote/main.py
+++ takenote/main.py
@@ -5,8 +5,15 @@ from lib import zettelkasten
 
 
 @click.command()
-@click.argument("note", nargs=1, type=str)
-@click.option("-t", "--title", "title", type=str)
+@click.argument("note", nargs=1, type=str, required=False)
+@click.option(
+    "-t",
+    "--title",
+    "title",
+    type=str,
+    default=None,
+    help="Sets the filename of a note.",
+)
 def cli(note: str, title: str) -> None:
     """
     The note has to be wrapped in quotes for single line.
@@ -17,25 +24,30 @@ def cli(note: str, title: str) -> None:
     tn "Note"
 
     """
-    # Generate settings
-    create_config_file()
+    click.secho("Take note!", fg="green")
+    try:
+        # Generate settings
+        create_config_file()
+        output_dir = Path(settings["save_path_notes"])
 
-    print("Take note!")
+        if title is not None:
+            file_path = output_dir / title
+        else:
+            # Can't save a file without a title/filepath.
+            zettel = zettelkasten()
+            click.echo(f"no title provided, saved under {zettel} instead")
+            file_path = output_dir / zettel
 
-    output_dir = Path(settings["save_path_notes"])
+        if note is None:
+            note = click.edit()
 
-    if title is not None:
-        file_path = output_dir / title
-    else:
-        # Can't save a file without a title/filepath.
-        zettel = zettelkasten()
-        print(f"no title provided, saved under {zettel} instead")
-        file_path = output_dir / zettel
+        with file_path.open("w") as file:
+            file.write(note)
 
-    with open(file_path, "w") as file:
-        file.write(note)
+        click.secho("Note saved successfully!", fg="green")
 
-    print("Note saved successfully!")
+    except Exception as e:
+        click.secho(f"Error occured:\n{e}", fg="red")
 
 
 if __name__ == "__main__":
