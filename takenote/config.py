8788d4ced86e22bf347f1cc3b20e471846e3c23c
proximalf <proximalfuture@gmail.com>
2022-11-12 12:58:09 +0000

    refactor: moved validators out of global scope

--- takenote/config.py
+++ takenote/config.py
@@ -2,15 +2,6 @@ from pathlib import Path
 from typing import List
 from dynaconf import Dynaconf, Validator
 
-validators = [
-    Validator("EDITOR", must_exist=True, default=None),
-    Validator("EXTENSION", must_exist=True, default="md"),
-    Validator("SAVE_PATH_NOTES", must_exist=True, default="./"),
-    Validator("TEMPLATES_DIR", must_exist=True, default="./templates"),
-    Validator("FORMAT.filename", must_exist=True, default={"long": "{{ title }}", "short": "new-note"}),
-    Validator("VERBOSITY_LEVEL", must_exist=True, default=1),
-]
-
 
 def generate_config_file(template_file: Path, filepath: Path) -> None:
     """
@@ -32,6 +23,7 @@ def generate_config_file(template_file: Path, filepath: Path) -> None:
 def config_file(filepaths: List[Path]) -> Dynaconf:
     """
     Returns an existing config file. Requires a list of files for input.
+    Files are overwritten based on order in list.
 
     Parameters
     ----------
@@ -43,6 +35,16 @@ def config_file(filepaths: List[Path]) -> Dynaconf:
     Dict[str, Any]
         Settings object from Dynaconf.
     """
+
+    validators = [
+        Validator("EDITOR", must_exist=True, default=None),
+        Validator("EXTENSION", must_exist=True, default="md"),
+        Validator("SAVE_PATH_NOTES", must_exist=True, default="./"),
+        Validator("TEMPLATES_DIR", must_exist=True, default="./templates"),
+        Validator("FORMAT.filename", must_exist=True, default={"long": "{{ title }}", "short": "new-note"}),
+        Validator("VERBOSITY_LEVEL", must_exist=True, default=1),
+    ]
+
     settings = Dynaconf(
         envvar_prefix="TN",
         settings_files=filepaths,
